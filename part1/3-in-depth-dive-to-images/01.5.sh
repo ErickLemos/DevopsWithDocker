18eaff33ff0879fc8bc38e7fc257226bedc7f423
Erick Lemos <ericklemoscosta@gmail.com>
2022-02-14 15:23:28 -0300

    01.5.sh

---
+++ part1/3-in-depth-dive-to-images/01.5.sh
@@ -0,0 +1,7 @@
+docker pull devopsdockeruh/simple-web-service:ubuntu
+docker pull devopsdockeruh/simple-web-service:alpine
+docker images
+
+result:
+devopsdockeruh/simple-web-service    ubuntu                                                  4e3362e907d5   11 months ago   83MB
+devopsdockeruh/simple-web-service    alpine                                                  fd312adc88e0   11 months ago   15.7MB
